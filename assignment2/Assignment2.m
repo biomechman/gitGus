ffd5797e4c50dd50141b6944401880fbc09ddf09
Gustavo Sandri Heidner <sandriheidnerg17@students.ecu.edu>
2017-08-29 11:56:04 -0400

    v0.1

---
+++ assignment2/Assignment2.m
@@ -0,0 +1,95 @@
+
+%% Chapter 1 Exercises
+
+% 1.)
+
+weightCu = 63.55
+
+% 2.)
+
+myage = 31
+myage = myage - 2
+myage = myage + 1
+
+% 3.)
+
+namelengthmax
+
+% 4.)
+
+weightLbs = 1.0
+weightOz = 16.0
+who
+whos
+clear weightLbs
+who
+whos
+
+% 5.)
+
+intmin('uint32')
+intmax('uint32')
+
+intmin('uint64')
+intmax('uint64')
+
+% 6.)
+
+decimalNum = 10.1
+decimalNumInt32 = int32(decimalNum)
+
+% 11.)
+
+pounds = 134.5
+kilos = pounds * 2.2
+
+% 12.)
+
+ftemp = -10 % Stores the approximate heart temperature of my ex (in Farenheit).
+ctemp = (ftemp - 32)*5/9
+
+% 13.)
+
+liters = 1.5
+flOz = liters * 33.814
+
+% 14.)
+
+sind(90)
+sin(pi/2)
+
+% 15.)
+
+R1 = 1
+R2 = 1
+R3 = 1
+
+RT = 1/(1/R1 + 1/R2 + 1/R3)
+
+% 22.)
+
+double('a')
+double('A')
+fprintf('Capitalized letters come first.\n')
+
+% 24.)
+
+logical 1
+logical 1
+2
+logical 0
+
+% 25.)
+
+x = 3
+y = 9
+
+xor(x > 5, y < 10)
+    
+% 26.)
+
+3*10^5 == 3e5
+
+% 27.)
+
+log10(10000) == 4
