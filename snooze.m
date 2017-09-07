d8893656ca08d82d84844ca73108655c4e27bafe
Gustavo Sandri Heidner <sandriheidnerg17@students.ecu.edu>
2017-09-07 12:08:30 -0400

    The Morning Alarm Chonundrum

---
+++ snooze.m
@@ -0,0 +1,13 @@
+sleepy = true;
+snoozeLimit = 1;
+while sleepy
+    fprintf('ALARM %.0f!!!\n\n', snoozeLimit)
+    fprintf('Snoozing')
+    pause(1), fprintf('.'), pause(1), fprintf('.'), pause(1), fprintf('.'), pause(1), fprintf('.'), pause(1), fprintf('.\n\n')
+    snoozeLimit = snoozeLimit + 1;
+    if snoozeLimit == 6
+        sleepy = false;
+        fprintf('Waking up!\n\n')
+    end
+end
+    
